ff3ed668989219de01ded36bc5ea4587c5456893
Matt Malinowski <matt@emersonmedia.com>
2015-04-21 10:36:12 -0400

    Add outline for asset inclusion warn logs

--- src/helpers.php
+++ src/helpers.php
@@ -40,16 +40,23 @@ if ( ! function_exists('build_assets'))
      */
     function build_assets($dependencies = [], $key, $extension)
     {
+        // TODO: This function "fails" silently. There is default behavior that
+        // we can expect with frontend assets, and we should log when we don't
+        // see that behavior. (It's possible that this non-default behavior is
+        // desired, but this is the exception!)
+
         $assets = [];
 
         // Get build configs
         $file = 'esensi/assembler::assembler';
         $builds_dir = public_path(config($file.'.directories.base', 'builds')) . config($file.'.directories.' . $key, $key);
+        // TODO: Log::warn if $builds_dir does not exist.
         $builds_url = asset(config($file.'.directories.base', 'builds')) . config($file.'.directories.' . $key, $key);
 
         // Compile the manifest files
         $manifest_file = $builds_dir . '/rev-manifest.json';
         if( ! file_exists($manifest_file) ) return;
+        // TODO: Log::warn if $manifest_file doesn't exist.
         $manifest = json_decode(file_get_contents($manifest_file), true);
 
         // Babysit dependencies so we don't have duplications
@@ -62,6 +69,7 @@ if ( ! function_exists('build_assets'))
             $dependency .= '.' . $extension;
 
             // Only include dependencies that are built
+            // TODO: Log::warn if a dependency is requested but not built.
             if (isset($manifest[$dependency]))
             {
                 // Get the latest revision
@@ -71,10 +79,12 @@ if ( ! function_exists('build_assets'))
                 switch($key)
                 {
                     case 'styles':
+                        // TODO: Log::warn if this file does not exist.
                         $assets[] = '<link rel="stylesheet" href="' . $builds_url . '/' . $revision .'">';
                         break;
 
                     case 'scripts':
+                        /// TODO: Log::warn if this file does not exist.
                         $assets[] = '<script type="text/javascript" src="' . $builds_url . '/' . $revision .'"></script>';
                         break;
                 }
